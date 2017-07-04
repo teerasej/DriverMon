40dc41b29d258de110cdf945d4a7734da80f0641
Teerasej Jiraphatchandej <teerasej@gmail.com>
2017-07-04 14:38:38 +0700

    คอมมิทครั้งแรก

---
+++ MyAppPon/MyAppPon/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MyAppPon
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            String username = entryUsername.Text;
+            var password = entryPassword.Text;
+            var email = entryEmail.Text;
+
+            DisplayAlert(
+                "ข้อมูลผู้ใช้"
+                , "Username: " + username + ", Email: " + email
+                , "ยกเลิก");
+        }
+    }
+}
